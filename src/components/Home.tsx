ddf035c6435c2b48a325fd0f63c4cef627b7d2f0
Kim Myungseong <forwarm5891@gmail.com>
2022-08-19 06:56:31 +0900

    fix: hangle test

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -12,7 +12,7 @@ const Home = () => {
   
   return (
     <div className='container mx-auto'>
-      <Link className='w-40 h-40 bg-rose-700' to="/beaches/해운대" />
+      <Link className='w-40 h-40 bg-rose-700' to="/beaches/해운대">해운대 test</Link>
       <Banner />
       <main className="grid grid-cols-2 h-[700px]">
         
