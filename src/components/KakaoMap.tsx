33bb79bb5d6ccffbe3d79434e1f31a4fcc793546
Kim Myungseong <forwarm5891@gmail.com>
2022-08-19 05:57:30 +0900

    fix: write encodeURICOMPONENT for deploy

--- src/components/KakaoMap.tsx
+++ src/components/KakaoMap.tsx
@@ -531,7 +531,7 @@ const KakaoMap = () => {
 
       goToReviewButton?.addEventListener('click',() => {
         console.log('excuted')
-        location.href = `${DEPLOY_URL}/beaches/${place.sta_nm}`
+        location.href = `${DEPLOY_URL}/beaches/${encodeURIComponent(place.sta_nm)}`
       })
        overlayTabsList.forEach((item) => {
         item.addEventListener('click',(item:any) => {
