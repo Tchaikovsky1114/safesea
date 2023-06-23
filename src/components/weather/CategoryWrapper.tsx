e1de7aa588b7197d6a6b672eae6adcf844979954
tchaikovsky1114 <forwarm5891@gmail.com>
2023-06-23 22:52:50 +0900

    refactoring: 재사용 컴포넌트로 전환

---
+++ src/components/weather/CategoryWrapper.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface Props {
+  children: React.ReactNode;
+  wrapperStyle?: string;
+}
+
+function CategoryWrapper({children,wrapperStyle = 'justify-center'}:Props) {
+  return (
+    <div className={`min-w-[140px] min-h-[100px] flex flex-col items-center gap-2 ${wrapperStyle}`}>{children}</div>
+  )
+}
+
+export default CategoryWrapper
--- src/components/weather/WeatherDetails.tsx
+++ src/components/weather/WeatherDetails.tsx
@@ -1,9 +1,9 @@
 import useTime from '../../hooks/useTime';
 import { WeatherDetailsProps } from '../../types/interface/weather';
-import EmphasisSmallText from '../common/EmphasisSmallText';
+import EmphasisSmallText from '../common/text/EmphasisSmallText';
 import SmallImageCard from '../common/card/SmallImageCard';
-
-
+import CategoryWrapper from './CategoryWrapper';
+import SmallStatusLineText from '../common/text/SmallStatusLineText';
 
 const WeatherDetails = ({forecastDate,forecastCategory,forecastTime,forecastValue}:WeatherDetailsProps) => {
   const {hours,today, minutes} = useTime()  
@@ -12,21 +12,21 @@ const WeatherDetails = ({forecastDate,forecastCategory,forecastTime,forecastValu
 
   if(forecastCategory === "TMP"){
     return (
-      <div className='min-w-[140px] min-h-[100px] flex flex-col items-center justify-start gap-2'>
+      <CategoryWrapper wrapperStyle='justify-start'>
         {
         forecastTime === "0000" && forecastDate.substr(4,1) === '0'
-        ? <p className="font-bold text-xs bg-blue-200 w-full bg-opacity-60 ">{forecastDate.substr(5,1)}월 {forecastDate.substr(6,1) === '0' ? forecastDate.substr(7) : forecastDate.substr(6) }일</p>
-        : <p className="font-bold text-xs bg-blue-200 w-full bg-opacity-60 ">{forecastDate.substr(4,2)}월 {forecastDate.substr(6,1) === '0' ? forecastDate.substr(7) : forecastDate.substr(6) }일</p>
+        ? <SmallStatusLineText>{forecastDate.substr(5,1)}월 {forecastDate.substr(6,1) === '0' ? forecastDate.substr(7) : forecastDate.substr(6) }일</SmallStatusLineText>
+        : <SmallStatusLineText>{forecastDate.substr(4,2)}월 {forecastDate.substr(6,1) === '0' ? forecastDate.substr(7) : forecastDate.substr(6) }일</SmallStatusLineText>
         }
        <EmphasisSmallText currentForecast={currentForecast} forecastDate={forecastDate} today={today}>{forecastTime.substr(0,2)}:00</EmphasisSmallText>
          <EmphasisSmallText currentForecast={currentForecast} forecastDate={forecastDate} today={today}>{forecastValue}°C</EmphasisSmallText>
-      </div>
+      </CategoryWrapper>
     );
   }
 
   if(forecastCategory === "PCP"){
     return (
-      <div className='min-w-[140px] min-h-[100px] flex flex-col items-center justify-center gap-2'>
+      <CategoryWrapper>
        {
        forecastValue === "강수없음"
         ? <SmallImageCard alt='맑음' src='/sunshine.png' text='없음' textStyle='text-[12px] font-bold' />
@@ -35,34 +35,34 @@ const WeatherDetails = ({forecastDate,forecastCategory,forecastTime,forecastValu
             { Number(forecastValue.split('.')[0]) > 3 && Number(forecastValue.split('.')[0]) < 15 && <SmallImageCard alt='비' src='/rainy.png' text='비' />}
             { Number(forecastValue.split('.')[0]) >= 15 &&<SmallImageCard alt='강한 비' src='/rainy.png' text='강한 비' /> }
           </EmphasisSmallText> }
-      </div>
+      </CategoryWrapper>
     );
   }
   if(forecastCategory === "POP"){
     return (
-      <div className='min-w-[140px] min-h-[100px] flex flex-col items-center justify-center gap-2'>
+      <CategoryWrapper>
         <EmphasisSmallText currentForecast={currentForecast} forecastDate={forecastDate} today={today}>
           {Number(forecastValue) >= 60 ? <><span className='text-[10px] font-bold'>강수 확률 {forecastValue}%! </span><span className='block text-[10px] pt-2'>우산 챙겨가세요! 🌂</span></>: <span>{forecastValue}%</span>}
         </EmphasisSmallText>
-      </div>
+      </CategoryWrapper>
     );
   }
  
   if(forecastCategory === "REH"){
     return (
-      <div className='min-w-[140px] min-h-[100px] flex flex-col items-center justify-center gap-2'>
+      <CategoryWrapper>
         <EmphasisSmallText currentForecast={currentForecast} forecastDate={forecastDate} today={today}>{forecastValue}%</EmphasisSmallText>
-      </div>
+      </CategoryWrapper>
     );
   }
   if(forecastCategory === "SKY"){
     return (
-      <div className='min-w-[140px] min-h-[100px] flex flex-col items-center justify-center gap-2'>
+      <CategoryWrapper>
          <EmphasisSmallText currentForecast={currentForecast} forecastDate={forecastDate} today={today}>
             <span className='text-xs font-bold'>{Number(forecastValue) < 4  && "청명한 하늘"}</span>
             <span className='text-xs font-bold'>{Number(forecastValue) >= 4  && "구름 많음"}</span>
           </EmphasisSmallText>
-      </div>
+      </CategoryWrapper>
     );
   }
   
